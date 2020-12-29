fa65873b88d14adf99f8f958edea4206130dc1bc
Tung <tungcan2000@gmail.com>
2020-12-30 00:07:06 +0700

    better

--- resources/views/genshintoy/tools/wish.blade.php
+++ resources/views/genshintoy/tools/wish.blade.php
@@ -33,13 +33,13 @@ https://gi-wish-simulator.uzairashraf.dev/0236cba43ee34e7100a781778c9f1bc3.png
 					<div class="col-md-12 banner d-flex" style="justify-content: center;height: 10%">
 						<div class="d-flex items banner-items carousel-indicators" style="justify-content: center;">
 							<div data-target="#carousel-banner" data-slide-to="0" class="item banner-item p-1 d-flex" style="width: 140px;height: auto;">
-								<img style="width: 100%;height: auto;    margin-top: auto;" src="https://i.imgur.com/F5czVNd.png" alt="" class="banner-image active" picked="https://i.imgur.com/MF0xV2K.png" unpicked="https://i.imgur.com/F5czVNd.png">
+								<img style="width: 100%;height: auto;    margin-top: auto;" src="https://i.imgur.com/F5czVNd.png" alt="" class="banner-image active" picked="https://i.imgur.com/MF0xV2K.png" unpicked="https://i.imgur.com/F5czVNd.png" title="Cầu nguyện nhân vật" data-fate ="intertwined">
 							</div>
 							<div data-target="#carousel-banner" data-slide-to="1" class="item banner-item p-1 d-flex" style="width: 140px;height: auto;">
-								<img style="width: 100%;height: auto;    margin-top: auto;" src="https://i.imgur.com/hM1Q35b.png" alt="" class="banner-image" picked="https://i.imgur.com/JHjzojm.png" unpicked="https://i.imgur.com/hM1Q35b.png">
+								<img style="width: 100%;height: auto;    margin-top: auto;" src="https://i.imgur.com/hM1Q35b.png" alt="" class="banner-image" picked="https://i.imgur.com/JHjzojm.png" unpicked="https://i.imgur.com/hM1Q35b.png" title="Cầu nguyện vũ khí" data-fate ="intertwined">
 							</div>
 							<div data-target="#carousel-banner" data-slide-to="2" class="item banner-item p-1 d-flex" style="width: 140px;height: auto;">
-								<img style="width: 100%;height: auto;    margin-top: auto;" src="https://i.imgur.com/9ScBoNk.png" alt="" class="banner-image" picked="https://i.imgur.com/Bc0V7s5.png" unpicked="https://i.imgur.com/9ScBoNk.png">
+								<img style="width: 100%;height: auto;    margin-top: auto;" src="https://i.imgur.com/9ScBoNk.png" alt="" class="banner-image" picked="https://i.imgur.com/Bc0V7s5.png" unpicked="https://i.imgur.com/9ScBoNk.png" title="Cầu nguyện thường" data-fate ="acquaint">
 							</div>
 						</div>
 					</div>
@@ -47,13 +47,13 @@ https://gi-wish-simulator.uzairashraf.dev/0236cba43ee34e7100a781778c9f1bc3.png
 						<div id="carousel-banner" class="carousel slide h-100" data-ride="carousel" data-interval="false" style="border-radius: 10px;padding: 0 140px;">
 						  <div class="carousel-inner h-100">
 						    <div class="carousel-item h-100 active">
-						      <img style="margin: auto;" class="d-block h-100" src="https://i.imgur.com/nZeYSmx.png" alt="First slide">
+						      <img style="margin: auto;" class="d-block h-100" src="https://i.imgur.com/nZeYSmx.png" alt="First slide" title="Cầu nguyện nhân vật">
 						    </div>
 						    <div class="carousel-item h-100">
-						      <img style="margin: auto;" class="d-block h-100" src="https://i.imgur.com/xZSr9gJ.png" alt="Second slide">
+						      <img style="margin: auto;" class="d-block h-100" src="https://i.imgur.com/xZSr9gJ.png" alt="Second slide" title="Cầu nguyện vũ khí">
 						    </div>
 						    <div class="carousel-item h-100">
-						      <img style="margin: auto;" class="d-block h-100" src="https://i.imgur.com/zbzKvvU.png" alt="Third slide">
+						      <img style="margin: auto;" class="d-block h-100" src="https://i.imgur.com/zbzKvvU.png" alt="Third slide" title="Cầu nguyện thường">
 						    </div>
 						  </div>
 						  <a style="width: 140px;position: absolute;left: 0;cursor: inherit;" class="carousel-control-prev carousel-control" href="#carousel-banner" role="button" data-slide="prev">
@@ -91,22 +91,33 @@ https://gi-wish-simulator.uzairashraf.dev/0236cba43ee34e7100a781778c9f1bc3.png
 									    box-shadow: 0px 0px 13px 0px #0000003d;margin: auto;">
 									<div style="line-height: 35px;">Detail</div>
 								</div>
-								<div class="fate ml-3" style="margin: auto;"></div>
-								<div class="primogems ml-3 d-flex pl-2 pr-3" style="background-color: #0000006e;color: white;height: 30px;border-radius: 20px;margin: auto;">
-									<img style="height: 100%;" src="https://static.wikia.nocookie.net/gensin-impact/images/d/d4/Item_Primogem.png" alt="">
-									<div style="margin: auto">9999999</div>
+								<div class="primogems ml-3 pl-2 pr-3 mats" style="background-color: #0000006e;color: white;height: 30px;border-radius: 20px;margin: auto;">
+									<img data-name='primogem' style="height: 100%;" src="https://static.wikia.nocookie.net/gensin-impact/images/d/d4/Item_Primogem.png" alt="">
+									<div class="item_amount" style="margin: auto"></div>
 									<div class="add-primogems">
-										
 									</div>
 								</div>
+								<div class="fates ml-3 pl-2 pr-3 mats" style="background-color: #0000006e;color: white;height: 30px;border-radius: 20px;margin: auto;">
+									<img data-name='acquaint' style="height: 100%;" src="https://static.wikia.nocookie.net/gensin-impact/images/2/22/Item_Acquaint_Fate.png"alt="">
+									<div class="item_amount" style="margin: auto"></div>
+									<div class="add-fates">
+									</div>
+								</div>
+								<div class="fates ml-3 pl-2 pr-3 mats" style="background-color: #0000006e;color: white;height: 30px;border-radius: 20px;margin: auto;">
+									<img data-name='intertwined' style="height: 100%;" src="https://static.wikia.nocookie.net/gensin-impact/images/1/1f/Item_Intertwined_Fate.png" alt="">
+									<div class="item_amount" style="margin: auto"></div>
+									<div class="add-fates">
+									</div>
+								</div>
+
 							</div>
 							<div class="right-side-btn d-flex" style="width: fit-content;">
 								<div class="button gacha" data-type="one" style="height: 55px;padding: 0 10px;position: relative;">
 									<img style="height: 100%;" src="https://i.imgur.com/jcI5szL.png">
 									<div class="content" style="position: absolute;top: 50%;left: 50%;transform: translate(-50%,-50%);height: 90%; color: #c3b2a3; font-size: 18px;">
 										<div style="line-height: 20px;">Wish x1</div>
-										<div class="d-flex" style="width: 100%;height: calc(100% - 20px);">
-											<img style="max-height: 100%;" src="https://static.wikia.nocookie.net/gensin-impact/images/1/1f/Item_Intertwined_Fate.png" alt="">
+										<div class="d-flex fate_type" style="width: 100%;height: calc(100% - 20px);">
+											<img style="max-height: 100%;" alt="">
 											<div style="margin: auto 0;">x1</div>
 										</div>
 									</div>
@@ -115,8 +126,8 @@ https://gi-wish-simulator.uzairashraf.dev/0236cba43ee34e7100a781778c9f1bc3.png
 									<img style="height: 100%;" src="https://i.imgur.com/jcI5szL.png">
 									<div class="content" style="position: absolute;top: 50%;left: 50%;transform: translate(-50%,-50%);height: 90%; color: #c3b2a3; font-size: 18px;">
 										<div style="line-height: 20px;">Wish x10</div>
-										<div class="d-flex" style="width: 100%;height: calc(100% - 20px);">
-											<img style="max-height: 100%;" src="https://static.wikia.nocookie.net/gensin-impact/images/1/1f/Item_Intertwined_Fate.png" alt="">
+										<div class="d-flex fate_type" style="width: 100%;height: calc(100% - 20px);">
+											<img style="max-height: 100%;" alt="">
 											<div style="margin: auto 0;">x10</div>
 										</div>
 									</div>
@@ -130,6 +141,21 @@ https://gi-wish-simulator.uzairashraf.dev/0236cba43ee34e7100a781778c9f1bc3.png
 @section('custom-script')
 <script>
 	const rarity = [4,5];
+	var fate_type;
+	var currentbanner = {
+		'fate_type' : null,
+		'name' : null,
+		'index' : null
+	};
+	var fate_type_icon ={
+		'intertwined' : "https://static.wikia.nocookie.net/gensin-impact/images/1/1f/Item_Intertwined_Fate.png",
+		'acquaint' : "https://static.wikia.nocookie.net/gensin-impact/images/2/22/Item_Acquaint_Fate.png"
+	}
+	var amount = {
+		'primogem': 999999,
+		'intertwined': 99999,
+		'acquaint': 99999
+	};
 	function randomArray(a){
 		if(Math.floor(Math.random()*a.length) >= a.length)
 			return randomArray(a);
@@ -140,16 +166,41 @@ https://gi-wish-simulator.uzairashraf.dev/0236cba43ee34e7100a781778c9f1bc3.png
 <script>
 	$(document).ready(function(){
 		$('body').css('height','100vh').css('overflow','hidden').append('<div class="skip-btn">Skip</div>');
+		$('body').append('<a href=' + '{{ route('genshintoy.index') }}' + '><div class="exit-btn"><img style="height:100%;width:auto;" src="https://i.imgur.com/IXIxQ3Y.png"></div></a>');
+
+		$('body').append('<div class="banner_name"><img style="height:100%;width:auto;" src="https://i.imgur.com/IfbxpWX.png"><p></p></div>');
+
 		$('.container').addClass('container-fluid').removeClass('container').css('width','100vw').css('height','100vh');
+
+		function updatebannername(){
+			$('.banner_name p').text(currentbanner['name']);
+		}
+		function updateicon(){
+			$('.gacha .fate_type img').attr('src',fate_type_icon[currentbanner['fate_type']]);
+			$.each($('.fates'),function(){
+				$(this).toggle($(this).find('img').attr('data-name').toLowerCase() == currentbanner['fate_type']);
+			});
+		}
+		function updateamount(){
+			$.each($('.mats'),function(){
+				$(this).find('.item_amount').text(amount[$(this).find('img').attr('data-name')]);
+			})
+		}
 		function updatebanner(){
 			$.each($('.banner-item img'),function(){
-				if($(this).hasClass('active'))
+				if($(this).hasClass('active')){
 					$(this).attr('src',$(this).attr('picked'));
+					currentbanner['name']= $(this).attr('title');
+					currentbanner['fate_type'] = $(this).attr('data-fate').toLowerCase();
+				}
 				else
 					$(this).attr('src',$(this).attr('unpicked'));
 			})
-		}
+			updateicon();
+			updatebannername();
+			updateamount();
 
+		}
 		$('.banner-item img').on('click',function(){
 			$.each($('.banner-item img'),function(){
 				$(this).removeClass('active');
